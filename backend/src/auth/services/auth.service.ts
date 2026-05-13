f18748107fa8a63ab8b045a99bba052d3c1d9842
SiyaSibiya <u23976072@tuks.co.za>
2026-05-13 22:52:19 +0200

    downgraded prisma(linting)

--- backend/src/auth/services/auth.service.ts
+++ backend/src/auth/services/auth.service.ts
@@ -1,32 +1,35 @@
-import { Injectable } from "@nestjs/common";
-import * as bcrypt from "bcrypt";
-import { PrismaService } from "../../common/prisma.service";
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../../common/prisma.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async hashpassword(password: string) : Promise<string>{
-        const salt = await bcrypt.genSalt(10);
-        return await bcrypt.hash(password, salt);
+  async hashpassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  }
+
+  async validatePassword(
+    email: string,
+    password: string,
+  ): Promise<object | null> {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return null;
     }
 
-    async validatePassword(email: string, password: string) {
-        const user = await this.prisma.user.findFirst({
-            where: {
-                email
-            }
-        });
-        if (!user) {
-            return null;
-        }
+    const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
 
-        const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
-
-        if (!isPasswordValid) {
-            return null;
-        }
-
-        return user;
+    if (!isPasswordValid) {
+      return null;
     }
+
+    return user;
+  }
 }

a18e561221e06e9f7f0a0ae18d257b045578e6b1
SiyaSibiya <u23976072@tuks.co.za>
2026-05-13 19:23:44 +0200

    hashed password and bcrypt auth service

---
+++ backend/src/auth.services.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
+import { PrismaService } from "./common/prisma.service";
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
+    async hashpassword(password: string) : Promise<string>{
+        const salt = await bcrypt.genSalt(10);
+        return await bcrypt.hash(password, salt);
+    }
+
+    async validatePassword(email: string, password: string) {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email
+            }
+        });
+        if (!user) {
+            return null;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
+
+        if (!isPasswordValid) {
+            return null;
+        }
+
+        return user;
+    }
+}

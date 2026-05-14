94ae6f0b307b4a8af9dd0f85cf93294256f62ed8
retshepile.nkwana <retshepile.nkwana@entelect.co.za>
2026-05-14 12:21:56 +0200

    Added the user account activatiom DTO used during registration.

---
+++ backend/src/auth/dto/activate-account.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+import { IsStrongEnough } from '../../common/validators/is-strong-enough.validator';
+
+export class ActivateAccountDto {
+  @IsEmail({}, { message: 'A valid email address is required.' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Activation token is required.' })
+  token: string;
+
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Password must be at least 8 characters and include uppercase, lowercase, a number, and a special character.',
+    },
+  )
+  @IsStrongEnough()
+  password: string;
+}

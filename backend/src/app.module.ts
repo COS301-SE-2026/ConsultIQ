cda7f97be3e29ea04e21909245c0bd2f5ad8da4d
RNkwana <u23580578@tuks.co.za>
2026-05-14 19:38:09 +0200

    Added email service to its module and refactoring.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { EmailModule } from './email/email.module';
 import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { CommonModule } from './common/common.module';
     PrismaModule,
     EmailModule,
     CommonModule,
+    AuthModule
   ],
 })
 export class AppModule {}

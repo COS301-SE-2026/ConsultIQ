8d6da2e5c1ff61e3608ef962af5ba342aea05088
RNkwana <u23580578@tuks.co.za>
2026-05-15 12:40:07 +0200

    Added Throttler Module import.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { PrismaModule } from './prisma/prisma.module';
 import { EmailModule } from './email/email.module';
 import { CommonModule } from './common/common.module';
@@ -7,13 +9,24 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'resend',
+        ttl: 3600000,
+        limit: 3,
+      },
+    ]),
     PrismaModule,
     EmailModule,
     CommonModule,
-    AuthModule
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}

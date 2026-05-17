65c425e8492c447b855ebe60f19b2be90a7dc9d1
Onthatile-22659812 <u22659812@tuks.co.za>
2026-05-17 02:10:00 +0200

    wired in consultant list page

--- frontend/src/routes/app-routes.tsx
+++ frontend/src/routes/app-routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegisterUserPage from "../features/authentication/pages/register-user-page";
 import SetPasswordPage from "../features/authentication/pages/set-password-page";
 import PopiaConsentPage from "../features/authentication/pages/popia-consent-page";
+import ConsultantsPage from "../features/consultants/pages/consultant-list-page";
 
 function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ function AppRoutes() {
             <Route path="/register" element={<RegisterUserPage />} />
             <Route path="/set-password" element={<SetPasswordPage />} />
             <Route path="/popia-consent" element={<PopiaConsentPage />} />
+            <Route path="/consultants-manager" element={<ConsultantsPage />} />
         </Routes>
         </BrowserRouter>
     );

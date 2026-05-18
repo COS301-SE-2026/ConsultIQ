b3faf80a1340ea96a5811a18db7ef5719bf4b10e
Onthatile-22659812 <u22659812@tuks.co.za>
2026-05-18 05:37:17 +0200

    deleted validate page

--- frontend/src/routes/app-routes.tsx
+++ frontend/src/routes/app-routes.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import RegisterUserPage from "../features/authentication/pages/register-user-page";
-import { ValidationForm } from "../features/authentication/pages/validation-page";
 import { LoginForm } from "../features/authentication/pages/login-page";
 
 function AppRoutes() {
@@ -9,7 +8,6 @@ function AppRoutes() {
         <BrowserRouter>
         <Routes>
             <Route path="/register" element={<RegisterUserPage />} />
-            <Route path="/validate" element={<ValidationForm />} />
             <Route path="/login" element={<LoginForm />} />
         </Routes>
         </BrowserRouter>

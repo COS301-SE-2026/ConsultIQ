702ce6d5e47a8015273f831815dd332e62636203
Ramatsobane29 <u22636022@tuks.co.za>
2026-05-19 03:03:54 +0200

    feature: implement a reusable input

---
+++ frontend/src/components/ui/input.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+
+import { cn } from "../../lib/utils";
+
+export interface InputProps
+    extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+    ({ className, type = "text", ...props }, ref) => {
+        return (
+            <input
+                type={type}
+                className={cn(
+                    `
+                    flex
+                    h-12
+                    w-full
+                    rounded-xl
+                    border
+                    border-slate-300
+                    bg-white
+                    px-4
+                    py-2
+                    text-sm
+                    outline-none
+                    transition
+
+                    placeholder:text-slate-400
+
+                    focus:border-[#002D72]
+                    focus:ring-2
+                    focus:ring-[#002D72]/20
+
+                    disabled:cursor-not-allowed
+                    disabled:opacity-50
+                    `,
+                    className
+                )}
+                ref={ref}
+                {...props}
+            />
+        );
+    }
+);
+
+Input.displayName = "Input";
+
+export { Input };

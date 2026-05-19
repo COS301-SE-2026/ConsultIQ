e803b81d05811d53819e1c35ef81960cdb178872
Onthatile-22659812 <u22659812@tuks.co.za>
2026-05-19 09:01:26 +0200

    under construction page

--- frontend/src/routes/app-routes.tsx
+++ frontend/src/routes/app-routes.tsx
@@ -1,6 +1,4 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import PageTransition from "../components/layout/page-transition";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import RegisterUserPage from "../features/authentication/pages/register-user-page";
 import { LoginForm } from "../features/authentication/pages/login-page";
@@ -9,36 +7,23 @@ import PopiaConsentPage from "../features/authentication/pages/popia-consent-pag
 import ConsultantsPage from "../features/consultants/pages/consultant-list-page";
 import ProjectSpecificationPage from "../features/projects/pages/project-specification-page";
 import ProjectListPage from "../features/projects/pages/project-list-page";
-import { ProtectedRoute } from "./protected-route";
-
-function AnimatedRoutes() {
-  const location = useLocation();
-
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        {/* Public routes */}
-        <Route path="/login" element={<PageTransition><LoginForm /></PageTransition>} />
-        <Route path="/set-password" element={<PageTransition><SetPasswordPage /></PageTransition>} />
-        <Route path="/activate" element={<PageTransition><SetPasswordPage /></PageTransition>} />
-        <Route path="/popia-consent" element={<PageTransition><PopiaConsentPage /></PageTransition>} />
-
-        {/* Protected routes */}
-        <Route path="/register" element={<ProtectedRoute><PageTransition><RegisterUserPage /></PageTransition></ProtectedRoute>} />
-        <Route path="/consultants-manager" element={<ProtectedRoute><PageTransition><ConsultantsPage /></PageTransition></ProtectedRoute>} />
-        <Route path="/project-specification" element={<ProtectedRoute><PageTransition><ProjectSpecificationPage /></PageTransition></ProtectedRoute>} />
-        <Route path="/projects" element={<ProtectedRoute><PageTransition><ProjectListPage /></PageTransition></ProtectedRoute>} />
-      </Routes>
-    </AnimatePresence>
-  );
-}
+import UnderConstructionPage from "../features/consultants/pages/under-construction-page";
 
 function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <AnimatedRoutes />
-    </BrowserRouter>
-  );
-}
-
-export default AppRoutes;
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/register" element={<RegisterUserPage />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/set-password" element={<SetPasswordPage />} />
+            <Route path="/popia-consent" element={<PopiaConsentPage />} />
+            <Route path="/consultants-manager" element={<ConsultantsPage />} />
+            <Route path="/project-specification" element={<ProjectSpecificationPage />} />
+            <Route path="/projects" element={<ProjectListPage />} />
+            <Route path="/consultant-FAQ" element={<UnderConstructionPage/>}/>
+        </Routes>
+        </BrowserRouter>
+    );
+    }
+    
+    export default AppRoutes;

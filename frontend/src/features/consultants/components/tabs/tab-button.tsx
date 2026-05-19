173630dbbb64f6a9bf77f320ac510ef2fece0a78
Ramatsobane29 <u22636022@tuks.co.za>
2026-05-19 03:14:51 +0200

    feature: implement the profile tabs

---
+++ frontend/src/features/consultants/components/tabs/tab-button.tsx
@@ -0,0 +1,47 @@
+type Props = {
+    label: string;
+    active?: boolean;
+    onClick: () => void;
+};
+
+export default function TabButton({
+    label,
+    active,
+    onClick,
+}: Props) {
+    return (
+        <button
+            onClick={onClick}
+            className={`
+                relative
+                pb-4
+                text-[18px]
+                font-semibold
+                transition-colors
+                duration-200
+
+                ${
+                    active
+                        ? "text-[#002D72]"
+                        : "text-slate-500 hover:text-slate-700"
+                }
+            `}
+        >
+            {label}
+
+            {active && (
+                <div
+                    className="
+                        absolute
+                        left-0
+                        bottom-0
+                        h-[3px]
+                        w-full
+                        rounded-full
+                        bg-[#002D72]
+                    "
+                />
+            )}
+        </button>
+    );
+}

a20e49c8c3374d2834973d4df4f960be3f86e73f
Onthatile-22659812 <u22659812@tuks.co.za>
2026-05-21 10:37:54 +0200

    added missing fields to dto

--- backend/src/consultants/dto/consultant-profile.dto.ts
+++ backend/src/consultants/dto/consultant-profile.dto.ts
@@ -6,19 +6,36 @@ export class ConsultantSkillsDto {
 }
 
 export class ConsultantCertificationsDto {
-  title!: string;
-  issuingBody!: string;
+  title!: string; 
+  issuingBody!: string; 
+  startDate!: Date | null;
+    endDate!: Date | null;
   uploadedAt!: Date;
 }
+
+export class ExperienceDTO {
+  companyname!: string;
+  jobTitle!: string;
+  jobType!: string;
+  startDate!: Date;
+  endDate!: Date | null;
+  roleDescription!: string;
+  workModel! : string;
+}
+
 
 export class ConsultantProfileDto {
     id!: string;
     fullName!: string;
     email!: string;
+    phoneNumber!:string;
+    idNumber!:string;
+    nationality!:string;
     location!: string;
     costToCompany!: number;
     availability!: string;
     skills!: ConsultantSkillsDto[];
+    experience!: ExperienceDTO[];
     certificates!: ConsultantCertificationsDto[];
 }
 

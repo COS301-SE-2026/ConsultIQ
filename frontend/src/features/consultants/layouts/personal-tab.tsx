9de2f56145697c8dedb62f1005f9302d036bdcc0
RNkwana <u23580578@tuks.co.za>
2026-05-21 21:51:36 +0200

    Modified location form.

--- frontend/src/features/consultants/layouts/personal-tab.tsx
+++ frontend/src/features/consultants/layouts/personal-tab.tsx
@@ -1,12 +1,16 @@
 import ProfileInfoForm from "../components/personal/profile-info-form";
 import LocationForm from "../components/personal/location-form";
 
-export default function PersonalTab() {
-    return (
-        <div className="space-y-8">
-            <ProfileInfoForm />
-            <div className="h-6" />
-            <LocationForm />
-        </div>
-    );
+interface Props {
+  onComplete: () => void;
+}
+
+export default function PersonalTab({ onComplete }: Props) {
+  return (
+    <div className="space-y-8">
+      <ProfileInfoForm />
+      <div className="h-6" />
+      <LocationForm onComplete={onComplete} />
+    </div>
+  );
 }

6565f0311ec3166f74657456a2bedf36bbf45690
RNkwana <u23580578@tuks.co.za>
2026-05-21 18:54:31 +0200

    Removed repository and its references.

--- backend/src/consultants/consultants.module.ts
+++ backend/src/consultants/consultants.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConsultantController } from '../controllers/consultants/consultant.controller';
 import { ConsultantService } from './services/consultant.service';
-import { ConsultantRepository } from './repositories/consultant.repository';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-    controllers: [ConsultantController],
-    providers: [ConsultantService, ConsultantRepository, PrismaService],
+  controllers: [ConsultantController],
+  providers: [ConsultantService],
+  imports: [PrismaModule],
 })
 export class ConsultantsModule {}

948fa70c0e026f2c63c644cae20bb5b357becefa
Ofentse360 <ofentsemodika2@gmail.com>
2026-06-17 23:38:37 +0200

    Admin controllers

---
+++ backend/src/controllers/admin/admin.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Delete, Patch } from "@nestjs/common";
+import { AdminUserService } from "src/admin/users/services/admin.user.service";
+import { Role } from '../../auth/enums/role.enum';
+import { Roles } from "src/common/guards/roles.guard";
+import { Get, Param } from "@nestjs/common";
+
+
+@Controller('admin')
+export class AdminController {
+    constructor(private readonly adminUserService: AdminUserService) { }
+
+    @Get('users')
+    @Roles(Role.ADMIN)
+    async getAllUsers() {
+        return await this.adminUserService.getAllUsers();
+    }
+
+    @Delete('users/:userId')
+    @Roles(Role.ADMIN)
+    async deleteUser(@Param('userId') userId: string) {
+        return await this.adminUserService.deleteUser(userId);
+    }
+
+    @Patch('users/:userId/activate')
+    @Roles(Role.ADMIN)
+    async activateUser(@Param('userId') userId: string) {
+        return await this.adminUserService.activeUser(userId);
+    }
+
+    @Patch('users/:userId/suspend')
+    @Roles(Role.ADMIN)
+    async suspendUser(@Param('userId') userId: string) {
+        return await this.adminUserService.suspendUser(userId);
+    }
+
+
+
+}

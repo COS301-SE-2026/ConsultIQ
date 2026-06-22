7aa4ce6f7d9f37fe39ab1282d92234c260affa76
Ofentse360 <ofentsemodika2@gmail.com>
2026-06-22 18:37:38 +0200

    Notifiction controller

---
+++ backend/src/controllers/notification/notification.controller.ts
@@ -0,0 +1,38 @@
+import {
+    Controller,
+    Get,
+    Req,
+    Param,
+    Patch,
+} from '@nestjs/common';
+import { NotificationService } from '../../notification/service/notification.service'
+
+@Controller('notifications')
+export class NotificationController {
+    constructor(private readonly notificationService: NotificationService) { }
+
+    @Get()
+    async getNotifications(@Req() req: any) {
+        const userId = req.userId;
+
+        return await this.notificationService.getNotifications(userId);
+    }
+    @Patch('id:/read')
+    async markAsRead(@Param('id') id: string, @Req() req: any) {
+        const userId = req.userId;
+        const notificationId = req.id;
+
+        return await this.notificationService.markAsRead(notificationId, userId);
+    }
+
+    @Patch('read-all')
+    async markAllAsRead(@Req() req: any) {
+        const userId = req.userId;
+
+
+        return await this.notificationService.markAllAsRead(userId);
+    }
+
+}
+
+

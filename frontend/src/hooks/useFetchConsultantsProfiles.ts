76a1de884d44d2485e21244c53958bb5bc95451b
RNkwana <u23580578@tuks.co.za>
2026-07-27 16:52:11 +0200

    Linting disabling.

--- frontend/src/hooks/useFetchConsultantsProfiles.ts
+++ frontend/src/hooks/useFetchConsultantsProfiles.ts
@@ -166,6 +166,7 @@ export function useFetchConsultantProfile(
     }, [targetConsultantId, loggedInUserId,hasParams]);
 
     useEffect(() => {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       fetchProfile();
     }, [fetchProfile]);
     

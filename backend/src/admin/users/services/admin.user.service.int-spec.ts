064d5e5830316427a17588ac657eaa742432c9d9
Ofentse360 <ofentsemodika2@gmail.com>
2026-08-12 17:53:49 +0200

    feat: Added PrismaModule to integration tests for AdminUserService, MatchRunService, and ScoringService

--- backend/src/admin/users/services/admin.user.service.int-spec.ts
+++ backend/src/admin/users/services/admin.user.service.int-spec.ts
@@ -4,6 +4,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AdminUserService } from './admin.user.service';
 import { cleanDatabase } from '../../../../prisma/prisma-test-utils';
 import { AdminModule } from '../../admin.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 describe('AdminUserService - Integration Test', () => {
   let service: AdminUserService;
@@ -11,7 +12,7 @@ describe('AdminUserService - Integration Test', () => {
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
-      imports: [AdminModule],
+      imports: [AdminModule, PrismaModule],
     }).compile();
 
     service = moduleRef.get<AdminUserService>(AdminUserService);

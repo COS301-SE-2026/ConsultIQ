6d4e41c8fe77e866a1b10c17f507f6fd6ff0536f
Ofentse360 <ofentsemodika2@gmail.com>
2026-08-19 02:08:20 +0200

    Refactor: Update handleSubscribe method to be asynchronous and adjust related test case

--- backend/src/notification/service/notification.gateway-service.ts
+++ backend/src/notification/service/notification.gateway-service.ts
@@ -9,7 +9,8 @@ import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: '/notifications', cors: true })
 export class NotificationGateway
-  implements OnGatewayConnection, OnGatewayDisconnect {
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server!: Server;
 
@@ -22,13 +23,13 @@ export class NotificationGateway
   }
 
   @SubscribeMessage('subscribeToNOtifcations')
-  handleSubscribe(client: Socket, userId: string) {
+  async handleSubscribe(client: Socket, userId: string) {
     const roomName = `user_notifications_${userId}`;
-    client.join(roomName);
+    await client.join(roomName);
     return { event: 'subscribed', data: `Successfully joined ${roomName}` };
   }
 
-  async sendPushNotification(userId: string, payload: any): Promise<void> {
+  sendPushNotification(userId: string, payload: any): void {
     const roomName = `user_notifications_${userId}`;
     this.server.to(roomName).emit('new_notification', payload);
   }

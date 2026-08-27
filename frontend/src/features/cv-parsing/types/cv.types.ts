4fe2b2c1732c1eec14e01eb7af9a16f6abb03bb0
Ramatsobane29 <u22636022@tuks.co.za>
2026-08-28 01:00:43 +0200

    add cv types

---
+++ frontend/src/features/cv-parsing/types/cv.types.ts
@@ -0,0 +1,78 @@
+export type ExtractionStatus = | "PENDING" | "PROCESSING" | "COMPLETED" | "FAILED" | "REVIEW_REQUIRED";
+export interface CvFileStatus{
+    id: string;
+    fileName: string;
+    fileSize: string;
+    mimeType: string;
+    uploadStatus: string;
+    extractionStatus: ExtractionStatus;
+    parsedData : ParsedCvData | null;
+    updatedAt: string;
+}
+
+export interface CvUploadResponse{
+    cvFieldId: string;
+    message: string;
+}
+
+export interface ParsedSkill{
+    skillName: string;
+    yearsExperience: string;
+    extractionConfidence: string;
+}
+
+export interface ParsedExperience{
+    jobTitle: string;
+    companyName: string;
+    jobType: string;
+    workModel: string;
+    startDate: string;
+    endDate?: string;
+    description: string;
+}
+
+export interface ParsedCertification{
+    title: string;
+    issuingBody: string;
+    startDate?: string;
+    endDate?: string;
+}
+
+export interface ParsedEducation{
+    institution: string;
+    qualification: string;
+    fieldOfStudy?: string;
+    startDate?: string;
+    endDate?: string;
+}
+
+export interface ParsedContactInfo{
+    fullName?: string;
+    email?: string;
+    phone?: string;
+    nationality?: string;
+    addressLine1?: string;
+    addressLine2?: string;
+    suburd?: string;
+    city?: string;
+    province?: string;
+    postalCode?: string;
+}
+
+export interface ConfidenceScores{
+    contact: number;
+    skills: number;
+    experience: number;
+    certifications: number;
+    education: number;
+    overall: number;
+}
+
+export interface ParsedCvData{
+contact: ParsedContactInfo;
+skills: ParsedSkill[];
+experiences: ParsedExperience[];
+certifications: ParsedCertification[];
+education: ParsedEducation[];
+confidenceScores: ConfidenceScores;
+}

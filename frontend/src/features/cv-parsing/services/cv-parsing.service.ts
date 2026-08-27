8b4d1c0c9052f0acfa97d5b8deb0d583d454797f
Ramatsobane29 <u22636022@tuks.co.za>
2026-08-28 01:37:56 +0200

    add upload and url download services

---
+++ frontend/src/features/cv-parsing/services/cv-parsing.service.ts
@@ -0,0 +1,19 @@
+import { apiClient } from "../../../lib/api-client";
+import type { CvFileStatus, CvUploadResponse } from "../types/cv.types";
+
+export const cvParsingService = {
+    async upload(consultantId: String, file: File): Promise<CvUploadResponse>{
+        const formData = new FormData();
+        formData.append("file", file);
+        return apiClient.post<CvUploadResponse>(`/cv/upload/${consultantId}`, formData,);
+    },
+
+    async getDownloadUrl(cvField: string): Promise<{url: string}>{
+        return apiClient.get<{ url: string}>(`cv/${cvField}/url`)
+    },
+
+    async getStatus(cvFileId: string) : Promise<CvFileStatus>{
+        return apiClient.get<CvFileStatus>(`cv/${cvFileId}/status`);
+    },
+
+};

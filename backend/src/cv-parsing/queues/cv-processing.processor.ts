946c524957b83288ee1fa667031ed52552a1cced
RNkwana <u23580578@tuks.co.za>
2026-08-28 12:40:27 +0200

    Adding processor.

---
+++ backend/src/cv-parsing/queues/cv-processing.processor.ts
@@ -0,0 +1,16 @@
+import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Job } from 'bullmq';
+import { Injectable } from '@nestjs/common';
+import { CvExtractionService } from '../services/cv-extraction.service';
+
+@Processor('cv-processing')
+@Injectable()
+export class CvProcessingProcessor extends WorkerHost {
+  constructor(private readonly cvExtraction: CvExtractionService) {
+    super();
+  }
+
+  async process(job: Job<{ cvFileId: string }>): Promise<void> {
+    await this.cvExtraction.processExtraction(job.data.cvFileId);
+  }
+}

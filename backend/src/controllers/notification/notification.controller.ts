86a4c7a6b33638aac256ce6f164a52c60dc37665
Ofentse360 <ofentsemodika2@gmail.com>
2026-08-31 14:54:59 +0200

    Applied authentication on notification service guards

--- backend/src/controllers/notification/notification.controller.ts
+++ backend/src/controllers/notification/notification.controller.ts
@@ -1,39 +1,41 @@
-import { Controller, Get, Req, Param, Patch } from '@nestjs/common';
+import { Controller, Get, Req, Param, Patch, UnauthorizedException } from '@nestjs/common';
 import { NotificationService } from '../../notification/service/notification.service';
 
 @Controller('notifications')
 export class NotificationController {
-  constructor(private readonly notificationService: NotificationService) {}
+  constructor(private readonly notificationService: NotificationService) { }
 
   @Get()
   async getNotifications(@Req() req: any) {
     const userId = req.userId;
-
+    if (!userId) throw new UnauthorizedException('User ID is missing');
     return await this.notificationService.getNotifications(userId);
   }
 
   @Get('archived')
   async getArchivedNotifications(@Req() req: any) {
     const userId = req.userId;
-
+    if (!userId) throw new UnauthorizedException('User ID is missing');
     return await this.notificationService.getArchivedNotifications(userId);
   }
   @Patch(':id/read')
   async markAsRead(@Param('id') id: string, @Req() req: any) {
     const userId = req.userId;
-
+    if (!userId) throw new UnauthorizedException('User ID is missing');
     return await this.notificationService.markAsRead(id, userId);
   }
 
   @Patch('read-all')
   async markAllAsRead(@Req() req: any) {
     const userId = req.userId;
-
+    if (!userId) throw new UnauthorizedException('User ID is missing');
     return await this.notificationService.markAllAsRead(userId);
   }
 
   @Patch(':id/archive')
-  async archiveNotification(@Param('id') id: string) {
-    return await this.notificationService.archiveNotification(id);
+  async archiveNotification(@Param('id') id: string, @Req() req: any) {
+    const userId = req.user?.userId;
+    if (!userId) throw new UnauthorizedException('User ID is missing');
+    return await this.notificationService.archiveNotification(id, userId);
   }
 }

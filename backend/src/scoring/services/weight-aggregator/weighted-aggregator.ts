af1f1218d387f2bed782e8f5856eccd8b6bbe212
SiyaSibiya <u23976072@tuks.co.za>
2026-09-26 13:18:51 +0200

    Change availability status to project availability status
    Signed-off-by: SiyaSibiya <u23976072@tuks.co.za>

--- backend/src/scoring/services/weight-aggregator/weighted-aggregator.ts
+++ backend/src/scoring/services/weight-aggregator/weighted-aggregator.ts
@@ -86,7 +86,7 @@ export class WeightedAggregator {
       finalScore: round(sum * 100, FINAL_SCORE_DECIMAL_PLACES),
       factorBreakdown,
       isPlaced: consultant.isPlaced,
-      availabilityStatus: deriveAvailabilityStatus(factorBreakdown),
+      projectAvailabilityStatus: deriveProjectAvailabilityStatus(factorBreakdown),
     };
   }
 }
